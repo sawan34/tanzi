a3205a1d0d2b1810847f61966696ae43126d8e45
sawan kumar <sawankumar@sawans-MacBook-Air.local>
2015-07-29 22:04:11 +0530

    w

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'tanzi');
+define('DB_NAME', 'database_name');
 
 /** MySQL database username */
 define('DB_USER', 'root');
